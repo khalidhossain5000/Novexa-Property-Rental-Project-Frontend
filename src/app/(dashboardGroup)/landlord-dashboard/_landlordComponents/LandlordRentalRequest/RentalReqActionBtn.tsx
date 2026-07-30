bb1e1176f45a71291ce744b3ef92b1dbfde793db
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-07-30 23:36:31 +0600

    rental req btn action added

---
+++ src/app/(dashboardGroup)/landlord-dashboard/_landlordComponents/LandlordRentalRequest/RentalReqActionBtn.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { updateRentalReqStatus } from "@/app/(dashboardGroup)/_actions/propertyActions";
+import { useActionState } from "react";
+
+interface IRentalRequestActionsProps {
+  rentalReqId: string;
+}
+
+const RentalRequestActions = ({
+  rentalReqId,
+}: IRentalRequestActionsProps) => {
+  const [state, action, isPending] = useActionState(
+    updateRentalReqStatus.bind(null, rentalReqId),
+    false,
+  );
+
+  return (
+    <form action={action} className="flex gap-2 justify-end">
+
+      <button
+        type="submit"
+        name="status"
+        value="APPROVED"
+        disabled={isPending}
+        className="rounded-lg bg-emerald-100 px-3 py-2 text-xs font-semibold text-emerald-700 hover:bg-emerald-200 disabled:opacity-50"
+      >
+        {isPending ? "Updating..." : "Approve"}
+      </button>
+
+
+      <button
+        type="submit"
+        name="status"
+        value="REJECTED"
+        disabled={isPending}
+        className="rounded-lg bg-rose-100 px-3 py-2 text-xs font-semibold text-rose-700 hover:bg-rose-200 disabled:opacity-50"
+      >
+        Reject
+      </button>
+
+    </form>
+  );
+};
+
+export default RentalRequestActions;
