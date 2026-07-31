b5bb89d77a321f0b8317d40cd8da3e216b078804
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-07-31 20:10:06 +0600

    forbidden page jwt utils and proxy some added

---
+++ src/app/(publicGroup)/forbidden/page.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Forbidden = () => {
+  return (
+      <div className="container lg:max-w-full lg:px-12 mx-auto min-h-screen shadow-2xl bg-[#fd8d01]">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl 2xl:text-6xl font-poppins font-bold md:font-extrabold text-slate-100 text-center   py-6 lg:py-2 pt-9 lg:pt-5 lg:pb-6">
+        Oops! You Shall Not Pass!
+      </h1>
+      <h1 className="text-sm md:text-xl xl:text-3xl py-3 pt-2 2xl:pb-6  font-medium max-w-5xl mx-auto text-center font-playfair dark:text-slate-100 ">
+        Looks like this page is more secret than your browser history. Try going
+        back before Gandalf gets mad.
+      </h1>
+      <div className="imgs text-center py-9 lg:py-5  2xl:py-0">
+        <Image 
+        src="/forbidden/forbiddenImg.png"
+        alt="forbidden image"
+        width={500}
+        height={150}
+        className='  mx-auto lg:w-6/12'
+        />
+      </div>
+      <div className="w-full  text-center ">
+        <Link href="/">
+          <button className="hover:scale-110 transition duration-300 relative inline-flex h-12 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#16100f_0%,#f9a300_50%,#f9a300_100%)]"></span>
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-[#ffffff] dark:bg-slate-950 px-7 text-sm lg:text-xl lg:font-bold font-medium text-black dark:text-white backdrop-blur-3xl gap-2 undefined ">
+              Back Home
+            </span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Forbidden;
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,16 +1,83 @@
-import { NextRequest, NextResponse } from "next/server"
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { jwtUtils } from "./utils/jwt";
+import { getNewAccessToken } from "./service/refreshToken";
+import { JwtPayload } from "jsonwebtoken";
+
+const authRoutes = ["/login", "/register"];
+export async function proxy(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+
+  const cookieStore = await cookies();
+
+  let accessToken = req.cookies.get("accessToken")?.value;
+  const refreshToken = req.cookies.get("refreshToken")?.value;
+
+  let decodedAccessToken = accessToken
+    ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
+    : null;
+
+  const decodedRefreshToken = refreshToken
+    ? jwtUtils.verifyToken(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      )
+    : null;
+
+  if (!decodedAccessToken?.success && decodedRefreshToken?.success) {
+    //access token has expired but refresh token is valid, get new access token from backend
+    const result = await getNewAccessToken();
+
+    if (result.success) {
+      const newAccessToken = result.data.accessToken;
+
+      cookieStore.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24,
+        sameSite: "lax",
+      });
+
+      accessToken = newAccessToken;
+      decodedAccessToken = jwtUtils.verifyToken(
+        accessToken!,
+        process.env.JWT_ACCESS_SECRET as string,
+      );
+    }
+  }
+  let userRole = null;
+
+  if (!decodedAccessToken?.success) {
+    //token has expired or is invalid, clear the cookies
+    cookieStore.delete("accessToken");
+  }
+    if(decodedAccessToken?.success && decodedAccessToken.data){
+        userRole = (decodedAccessToken.data as JwtPayload).role;
+    }
 
 
-export async function proxy(request: NextRequest) {
- 
-  
-    return NextResponse.next()
+    //user is logged in and trying to access login or register page, redirect to dashboard or root home page
+    if(accessToken && authRoutes.includes(pathname)){
+        if(userRole === "TENANT"){
+            return NextResponse.redirect(new URL('/dashboard', req.url));
+        }else if(userRole === "ADMIN"){
+            return NextResponse.redirect(new URL('/admin-dashboard', req.url));
+        }else if(userRole === "LANDLORD"){
+            return NextResponse.redirect(new URL('/landlord-dashboard', req.url));
+        }else{
+            return NextResponse.redirect(new URL('/', req.url)); //unauth page can be called
+        }
+    }
+
+
+
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        // '/dashboard/:path*',
-        // '/admin-dashboard/:path*',
-        '/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)'
-    ],
-}
+  matcher: [
+    // '/dashboard/:path*',
+    // '/admin-dashboard/:path*',
+    "/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)",
+  ],
+};
