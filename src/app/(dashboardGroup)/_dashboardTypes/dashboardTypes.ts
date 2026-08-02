ff01eef7aaa001d6cd82b74cd6660e5ec30bf483
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-08-02 12:30:30 +0600

    tenant stats types added

--- src/app/(dashboardGroup)/_dashboardTypes/dashboardTypes.ts
+++ src/app/(dashboardGroup)/_dashboardTypes/dashboardTypes.ts
@@ -100,44 +100,17 @@ export interface IPaymentResponse{
   data:IPayment[]
 }
 
+//dashboards stats
 
-// {
-//     "success": true,
-//     "message": "Your all payment history is here",
-//     "data": [
-//         {
-//             "id": "5171884a-6600-4a63-9528-7ed557e453a5",
-//             "transactionId": "TRNX_ID_1785494561715",
-//             "totalAmount": "22000",
-//             "provider": "SSL_Commerz",
-//             "paidAt": "2026-07-31T10:42:43.649Z",
-//             "status": "PENDING",
-//             "rentalRequestId": "93968ab9-9256-4c03-88da-c690b16290ac",
-//             "rentalRequest": {
-//                 "id": "93968ab9-9256-4c03-88da-c690b16290ac",
-//                 "status": "ACTIVE",
-//                 "totalAmount": "22000",
-//                 "propertyId": "0f058886-128b-48a8-8b2a-d316f0e52838",
-//                 "tenantId": "494bbd0a-67f9-435a-a9b4-a9efd24b25b8",
-//                 "created_At": "2026-07-31T08:50:32.160Z",
-//                 "updated_At": "2026-07-31T10:56:05.415Z",
-//                 "property": {
-//                     "id": "0f058886-128b-48a8-8b2a-d316f0e52838",
-//                     "title": "Affordable Bachelor Flat",
-//                     "description": "One-bedroom flat located close to public transport and shopping centers.",
-//                     "location": "Mohammadpur, Dhaka",
-//                     "price": "22000",
-//                     "amenities": "WiFi, Security, Lift",
-//                     "thumbnailImage": "https://i.ibb.co.com/dJWwQNvF/mountain.jpg",
-//                     "status": "BOOKED",
-//                     "categoryId": "ba20a26c-e49d-48ba-b269-1dd29ebe2eb8",
-//                     "landLordId": "dff740e5-6de9-448b-95ff-6a899a9a4ebf",
-//                     "created_At": "2026-07-11T15:10:05.121Z",
-//                     "updated_At": "2026-07-31T10:56:05.478Z"
-//                 }
-//             }
-//         },
-     
- 
-//     ]
-// }
+export interface ITenantStats{
+  totalRequestSent:number;
+  pendingRequest:number;
+  rejectedRequest:number;
+  activeRent:number;
+}
+export interface ITenanstStatsRes{
+  success:boolean;
+  message:string;
+  data:ITenantStats
+}
+
